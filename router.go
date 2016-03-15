84122dca127252b358e0d47cd6970b4154cc746d
Marcel Hauf <mail@marcelhauf.name>
2016-03-15 16:08:40 +0100

    move Chain code into router.go

--- router.go
+++ router.go
@@ -127,3 +127,14 @@ func NewRouter() *Router {
 type Handler interface {
 	Handle(context.Context, Conn, Message) context.Context
 }
+
+// A Chain is a chain of Handlers. It implements a Handler.
+type Chain []Handler
+
+// Handle calls each chain element Handle function subsequently.
+func (c *Chain) Handle(ctx context.Context, conn Conn, msg Message) context.Context {
+	for i := range *c {
+		ctx = (*c)[i].Handle(ctx, conn, msg)
+	}
+	return ctx
+}
